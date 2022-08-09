18aff3c106c0a6803b92ea4e3eb804d0c913996d
kangkang2410 <haphongpk12@gmail.com>
2022-08-09 08:50:14 +0700

    Update service login signup social

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -13,6 +13,39 @@ const Users: CollectionConfig = {
     // Email added by default
     // Add more fields as needed
   ],
+  endpoints: [
+    {
+      path: 'login',
+      method: 'post',
+      handler: async(req, res, next) => {
+        const loginType = req.body.loginType;
+        const auth = passport.authenticate(loginType, {
+          scope: ['email'],
+          state: req.query.role
+        });
+      }
+    },
+    {
+      path: 'signup',
+      method: 'post',
+      handler: async(req, res, next) => {
+      }
+    },
+    {
+      path: 'facebook/callback',
+      method: 'get',
+      handler: async(req, res, next) => {
+
+      }
+    },
+    {
+      path: 'google/callback',
+      method: 'get',
+      handler: async(req, res, next) => {
+
+      }
+    },
+  ]
 };
 
 export default Users;
