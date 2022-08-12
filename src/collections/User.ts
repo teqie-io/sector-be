97d6c096ea4cc6d9dac9402ef8713e8933182859
Vo Minh Thien Long <thienlongtpct@gmail.com>
2022-08-12 12:39:14 +0700

    for deploying

--- src/collections/User.ts
+++ src/collections/User.ts
@@ -1,5 +1,7 @@
 import { CollectionConfig } from 'payload/types';
 
+const passport = require('passport');
+
 const User: CollectionConfig = {
   slug: 'user',
   auth: true,
@@ -14,41 +16,52 @@ const User: CollectionConfig = {
       name: 'fullName',
       type: 'text',
       required: true
+    },
+    {
+      name: 'role',
+      type: 'select',
+      options: [
+        { label: 'Admin', value: 'admin' },
+        { label: 'User', value: 'user' },
+      ],
+      required: true,
+      defaultValue: 'user',
     }
   ],
-  // endpoints: [
-  //   {
-  //     path: 'login',
-  //     method: 'post',
-  //     handler: async(req, res, next) => {
-  //       const loginType = req.body.loginType;
-  //       const auth = passport.authenticate(loginType, {
-  //         scope: ['email'],
-  //         state: req.query.role
-  //       });
-  //     }
-  //   },
-  //   {
-  //     path: 'signup',
-  //     method: 'post',
-  //     handler: async(req, res, next) => {
-  //     }
-  //   },
-  //   {
-  //     path: 'facebook/callback',
-  //     method: 'get',
-  //     handler: async(req, res, next) => {
-  //
-  //     }
-  //   },
-  //   {
-  //     path: 'google/callback',
-  //     method: 'get',
-  //     handler: async(req, res, next) => {
-  //
-  //     }
-  //   },
-  // ]
+  endpoints: [
+    {
+      path: '/login',
+      method: 'get',
+      handler: async(req, res, next) => {
+        // const loginType = req.body.loginType;
+        // const auth = passport.authenticate(loginType, {
+        //   scope: ['email'],
+        //   state: req.query.role
+        // });
+        res.status(200).send("demo");
+      }
+    },
+    // {
+    //   path: 'signup',
+    //   method: 'post',
+    //   handler: async(req, res, next) => {
+    //   }
+    // },
+    // {
+    //   path: 'facebook/callback',
+    //   method: 'get',
+    //   handler: async(req, res, next) => {
+    //
+    //   }
+    // },
+    // {
+    //   path: 'google/callback',
+    //   method: 'get',
+    //   handler: async(req, res, next) => {
+    //
+    //   }
+    // },
+  ]
 };
 
 export default User;
