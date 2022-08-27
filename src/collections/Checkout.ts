2e867edc3e5fb43c91cfa5e14303c63fd16a6c58
kangkang2410 <haphongpk12@gmail.com>
2022-08-27 10:55:07 +0700

    Update

--- src/collections/Checkout.ts
+++ src/collections/Checkout.ts
@@ -39,7 +39,47 @@ const Checkout: CollectionConfig = {
         {
             name: 'status',
             type: 'text'
-        }
+        },
+        {
+            name: 'firstName',
+            type: 'text',
+            required: true
+        },
+        {
+            name: 'lastName',
+            type: 'text',
+            required: true
+        },
+        {
+            name: 'address',
+            type: 'text',
+            required: true
+        },
+        {
+            name: 'city',
+            type: 'text',
+            required: true
+        },
+        {
+            name: 'state',
+            type: 'text',
+            required: true
+        },
+        {
+            name: 'postCode',
+            type: 'text',
+            required: true
+        },
+        {
+            name: 'email',
+            type: 'text',
+            required: true
+        },
+        {
+            name: 'phoneNumber',
+            type: 'text',
+            required: true
+        },
     ]
 }
 
--- src/controllers/checkout-controller.ts
+++ src/controllers/checkout-controller.ts
@@ -6,47 +6,19 @@ require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const createPaymentIntent = async (req: express.Request, res: express.Response, next) => {
-    // const checkout = req.body;
-    // let line_items = [];
-    // for(let i = 0; i < checkout.products.length; i++) {
-    //     const p = checkout.products[i];
-    //     line_items.push(
-    //         {
-    //             price_data: {
-    //                 currency: 'usd',
-    //                 product_data: {
-    //                     name: p.name,
-    //                     images: [p.image]
-    //                 },
-    //                 unit_amount: p.price
-    //             },
-    //             quantity: p.quantity
-    //         }
-    //     );
-    // }
     try {
-        // const session = await stripe.checkout.sessions.create(
-        //     {
-        //         payment_method_types: ['card'],
-        //         line_items,
-        //         mode: 'payment',
-        //         success_url: 'http://localhost:3001/checkout-result?status=success',
-        //         cancel_url: 'http://localhost:3001/checkout-result?status=cancel'
-        //     }
-        // );
-        // console.log(session);
-        // Create a PaymentIntent with the order amount and currency.
+        const amount = req.body.totalAmount;
+        const currency = req.body.currency;
+        const paymentMethodType = req.body.paymentMethodType;
+        console.log(req.body);
         const params: Stripe.PaymentIntentCreateParams = {
-            amount: 5999,
-            currency: 'usd',
-            payment_method_types: ['card'],
+            amount,
+            currency,
+            payment_method_types: [paymentMethodType],
         };
         const paymentIntent: Stripe.PaymentIntent = await stripe.paymentIntents.create(
             params
         );
-
-        console.log(paymentIntent)
-
 
         // Send publishable key and PaymentIntent client_secret to client.
         res.send({
@@ -55,7 +27,7 @@ const createPaymentIntent = async (req: express.Request, res: express.Response, 
         });
     } catch (ex) {
         payload.logger.error(ex);
-        
+
         res.status(400).send({
             error: {
                 message: ex.message,
